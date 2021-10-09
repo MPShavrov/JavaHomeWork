6520a94f23089f6d68d202331a87f73a6779dfd3
Maksim Shavrov <maksim.shavrov@privatbank.ua>
2021-10-09 20:03:50 +0300

    Доп

---
+++ src/com/pb/shavrov/hw1/HelloWorld2.java
@@ -0,0 +1,17 @@
+package com.pb.shavrov.hw1;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        text();
+        System.out.println(text1());
+    }
+
+    public static void text(){
+        System.out.println("Hello World");
+    }
+
+    public static String text1(){
+        String text = "Hello World";
+        return text;
+    }
+}
