e80774ab1aef7d840e49e82778200af4dce0695a
Maksim Shavrov <maksim.shavrov@privatbank.ua>
2021-10-15 21:43:08 +0300

    HW2 - целочисленные операнды

---
+++ src/com/pb/shavrov/hw2/Calculator1.java
@@ -0,0 +1,47 @@
+package com.pb.shavrov.hw2;
+
+import java.util.Scanner;
+
+public class Calculator1 {
+    public static void main(String[] args) {
+        int operand1 = 0;
+        int operand2 = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите действие (/, *, +, -)");
+        String sign = scan.next();
+        System.out.println("Введите 2 числа");
+        if (scan.hasNextDouble()) {
+            operand1 = scan.nextInt();
+        }
+        if (scan.hasNextInt()) {
+            operand2 = scan.nextInt();
+            calc(sign, operand1, operand2);
+        } else {
+            System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+        }
+    }
+
+    public static void calc(String sign, int operand1, int operand2) {
+        double val;
+        switch (sign) {
+            case "+":
+                System.out.println(operand1 + operand2);
+                break;
+            case "-":
+                System.out.println(operand1 - operand2);
+                break;
+            case "*":
+                System.out.println(operand1 * operand2);
+                break;
+            case "/":
+                if (operand2 == 0) {
+                    System.out.println("На 0 делит нельзя");
+                } else {
+                    System.out.println((double) operand1 / operand2);
+                }
+                break;
+            default:
+                System.out.println("Извините, но такого действия нет. Перезапустите программу и попробуйте снова!");
+        }
+    }
+}
