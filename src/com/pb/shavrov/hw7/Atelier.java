5a6b2da002873025475c2b07b990b3d59610b80a
Maksim Shavrov <maksim.shavrov@privatbank.ua>
2021-11-13 22:52:27 +0200

    HW7

--- src/com/pb/shavrov/hw7/Atelier.java
+++ src/com/pb/shavrov/hw7/Atelier.java
@@ -1,7 +1,24 @@
 package com.pb.shavrov.hw7;
 
+import static com.pb.shavrov.hw7.Atelier.dressMan;
+
 public class Atelier {
     public static void main(String[] args) {
 
+
+        Clothes clothes[] = {};
+
+        dressMan(clothes);
+        dressWomen(clothes);
+    }
+
+    public static String dressMan(Clothes[] clothes) {
+
+        return null;
+    }
+
+    public static String dressWomen(Clothes[] clothes) {
+
+        return null;
     }
 }
