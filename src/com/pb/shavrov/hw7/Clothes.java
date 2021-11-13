8887cf4cf26a61183b5560429cabeb885e00e299
Maksim Shavrov <maksim.shavrov@privatbank.ua>
2021-11-13 23:15:46 +0200

    HW7

--- src/com/pb/shavrov/hw7/Clothes.java
+++ src/com/pb/shavrov/hw7/Clothes.java
@@ -1,4 +1,16 @@
 package com.pb.shavrov.hw7;
 
 abstract class Clothes {
+    private String color;
+    private int sizw;
+    private int price;
+
+    public Clothes() {
+    }
+
+    public Clothes(String color, int sizw, int price) {
+        this.color = color;
+        this.sizw = sizw;
+        this.price = price;
+    }
 }
--- src/com/pb/shavrov/hw7/Pants.java
+++ src/com/pb/shavrov/hw7/Pants.java
@@ -1,6 +1,7 @@
 package com.pb.shavrov.hw7;
 
 public class Pants extends Clothes implements ManClothes,WomenClothes{
+
     @Override
     public void dressMan() {
 
