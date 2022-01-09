29e895e96869a707872a952e7d0cc6c68e500d95
Maksim Shavrov <maksim.shavrov@privatbank.ua>
2022-01-09 22:13:18 +0200

    HW14

---
+++ src/com/pb/shavrov/hw14/Server.java
@@ -0,0 +1,54 @@
+package com.pb.shavrov.hw14;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    static class Handler implements Runnable {
+        private final Socket socket;
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+                String msg;
+                while ((msg = in.readLine()) != null) {
+                    System.out.println(Thread.currentThread().getName() + ": " + msg);
+                    pw.println(LocalDateTime.now().toString() + " " + msg);
+                    if ("exit".equalsIgnoreCase(msg)) {
+                        break;
+                    }
+                }
+                System.out.println(Thread.currentThread().getName() + ": сеанс закончен");
+                pw.close();
+                in.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (Exception ex) {
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        int port = 1234;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server start on " + port + " port");
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            threadPool.submit(new Handler(clientSocket));
+        }
+    }
+}
