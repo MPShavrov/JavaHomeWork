663e1f31edae0fccdc01e9821efb99f0ec77877b
Maksim Shavrov <maksim.shavrov@privatbank.ua>
2022-01-17 07:50:47 +0200

    HW15

--- src/com/pb/shavrov/hw14/Client1.java
+++ src/com/pb/shavrov/hw14/Client1.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+public class Client1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Client start");
         Socket server = new Socket("127.0.0.1", 1234);
--- src/hw15/Serwer.java
+++ src/hw15/Serwer.java
@@ -1,4 +1,4 @@
-package com.pb.shavrov.hw14;
+package hw15;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -9,26 +9,29 @@ import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server {
+public class Serwer {
     static class Handler implements Runnable {
         private final Socket socket;
+
         public Handler(Socket socket) {
             this.socket = socket;
         }
+
         @Override
         public void run() {
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+
                 String msg;
                 while ((msg = in.readLine()) != null) {
+
                     System.out.println(Thread.currentThread().getName() + ": " + msg);
                     pw.println(LocalDateTime.now().toString() + " " + msg);
-                    if ("exit".equalsIgnoreCase(msg)) {
-                        break;
-                    }
                 }
-                System.out.println(Thread.currentThread().getName() + ": сеанс закончен");
+
+                System.out.println(Thread.currentThread().getName() + ": connection close");
+
                 pw.close();
                 in.close();
             } catch (Exception ex) {
@@ -41,11 +44,13 @@ public class Server {
             }
         }
     }
+
     public static void main(String[] args) throws Exception {
         int port = 1234;
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Server start on " + port + " port");
         ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
         while (true) {
             Socket clientSocket = serverSocket.accept();
             threadPool.submit(new Handler(clientSocket));
